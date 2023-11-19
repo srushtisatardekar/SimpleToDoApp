4db7c2f134f352d9b844af3440e5550b3a477223
srushti <srushti.satardekar10@gmail.com>
2023-11-19 01:28:08 -0800

    branch2 commit

--- src/ListScreen.js
+++ src/ListScreen.js
@@ -1,12 +1,35 @@
 // ListScreen.js
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Alert, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-const ListScreen = ({ items }) => {
+const ListScreen = ({ items, onEditItem, onDeleteItem }) => {
+  const handleEditItem = (item) => {
+    onEditItem(item);
+  };
+
+  const handleDeleteItem = (item) => {
+    Alert.alert(
+      'Confirm Deletion',
+      'Are you sure you want to delete this item?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          onPress: () => onDeleteItem(item),
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>List of Items</Text>
-      {items.length === 0 ? (
+      {items && items.length === 0 ? (
         <Text>No items available</Text>
       ) : (
         <FlatList
@@ -14,8 +37,25 @@ const ListScreen = ({ items }) => {
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <View style={styles.itemContainer}>
-              <Text style={styles.itemName}>{item.name}</Text>
-              <Text style={styles.itemDescription}>{item.description}</Text>
+              <TouchableOpacity
+                style={styles.itemTouchable}
+                onPress={() => handleEditItem(item)}
+              >
+                <Text style={styles.itemName}>{item.name}</Text>
+                <Text style={styles.itemDescription}>{item.description}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.iconContainer}
+                onPress={() => handleEditItem(item)}
+              >
+                <AntDesign name="edit" size={24} color="blue" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.iconContainer}
+                onPress={() => handleDeleteItem(item)}
+              >
+                <AntDesign name="delete" size={24} color="red" />
+              </TouchableOpacity>
             </View>
           )}
         />
@@ -35,7 +75,21 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    padding: 12,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginLeft: 20,
+  },
+  itemTouchable: {
+    flex: 1,
   },
   itemName: {
     fontSize: 18,
